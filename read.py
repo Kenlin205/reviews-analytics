d23c650d93517ef399535ea815fa36912e6d6916
Kenlin <kenlin205@gmail.com>
2019-06-09 10:57:41 +0800

    calcu ave

--- read.py
+++ read.py
@@ -4,9 +4,12 @@ with open("reviews.txt", "r") as f:
 	for line in f:
 		data.append(line)
 		count +=1 
-		if count % 1000 == 0:
+		if count % 10000 == 0:
 			print(len(data))
-print(len(data))
-# print(data[0])
-# print("------------------------")
-# print(data[1])
+print("all data have readed, There were ",len(data), "data")
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print("Every message have average",sum_len/len(data),"length")
+
+
