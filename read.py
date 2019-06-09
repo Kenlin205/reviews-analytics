21c0e2692e933313f42895cc413f7237f3b6d156
Kenlin <kenlin205@gmail.com>
2019-06-09 11:16:08 +0800

    add filter

--- read.py
+++ read.py
@@ -13,3 +13,12 @@ for d in data:
 print("Every message have average",sum_len/len(data),"length")
 
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print("There were ",len(new), "data less than 100")
+print("-------------------------below is message")
+print(new[0])
+print(new[1])
+
